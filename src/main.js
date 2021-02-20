b14f4867220dd56fb6347b4b241a996f3b30ff16
Gri.Lee <amistadlee10@gmail.com>
2021-02-20 22:35:21 +0900

    add filter function

--- src/main.js
+++ src/main.js
@@ -63,6 +63,29 @@ function scrollIntoView(selector) {
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
 
+// projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      let type = project.dataset.type;
+      if (filter === "*" || type === filter) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
+
 function init() {
   showNavBar();
   navbarMenu.addEventListener("click", BtnScrollIntoView);
